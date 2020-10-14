c10a371e8bbf4deb1c68c45562f2c6b1f25bc08d
Jacob Henson <Gammernut@aol.com>
2020-10-14 09:21:18 -0500

    Initial commit

---
+++ how_to_live_with_no_internet.py
@@ -0,0 +1,17 @@
+import utill
+
+
+def main():
+    utill.print_header('There is no internet and i am suffering')
+
+
+# start under here
+
+print()
+
+
+# end here
+
+
+if __name__ == '__main__':
+    main()
